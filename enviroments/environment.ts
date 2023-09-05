b73e7752a79c09c9224aa02f3773ff44611077b7
valerioprds <v.paradiso89@gmail.com>
2023-09-05 11:56:22 +0200

    first commit

---
+++ enviroments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  mapbox: {
+    accessToken:
+      'pk.eyJ1IjoidmFsZXJpb3ByZHMiLCJhIjoiY2xsM3kyYWc3MDE1cTNka2RuNWU5YTRobiJ9.ixrPJgwmaCl6V6hi5WcI-g',
+  },
+};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'enviroments/environment';
+import * as mapboxgl from 'mapbox-gl';
+import * as MapboxDirections from '@mapbox/mapbox-gl-directions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'map_directions';
+
+  map?: mapboxgl.Map; // Using a more concise optional type syntax
+  style = 'mapbox://styles/mapbox/streets-v11'; // Fixed the path here
+  lat = 12.0911;
+  lng = 85.8511;
+  zoom = 10;
+
+  //41.733540418351495, 12.286741957722258
+
+  constructor() {
+    (mapboxgl as any).accessToken = environment.mapbox.accessToken; // Fixed typo here
+  }
+
+  ngOnInit(): void {
+    this.buildMap();
+  }
+
+  buildMap() {
+    const navControl = new mapboxgl.NavigationControl({ visualizePitch: true });
+
+    let geolocate = new mapboxgl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true,
+      },
+      trackUserLocation: true,
+      showUserHeading: true,
+    });
+
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: this.style,
+      zoom: this.zoom,
+      center: [this.lng, this.lat],
+      attributionControl: false,
+    });
+
+    this.map.addControl(navControl, 'top-right');
+
+    this.map.addControl(geolocate, 'top-right');
+
+    geolocate.on('geolocate', locateUser);
+    this.map.on('load', function () {
+      geolocate.trigger();
+    });
+
+    // scaling control:
+    this.map.addControl(new mapboxgl.ScaleControl(), 'bottom-right');
+
+    // full screen control:
+    this.map.addControl(new mapboxgl.FullscreenControl(), 'top-right');
+  }
+}
+
+function locateUser(e: any) {
+  console.log('Lng :' + e.coords.longitude + '' + 'Lat :' + e.coords.latitude);
 }
